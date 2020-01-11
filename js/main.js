ca6011934542ee3b9ac3d2400eeb79446de20a2e
inCrafts <inCrafts@mail.ru>
2020-01-11 20:16:27 +0300

    Секция Events завершена

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+	$('.collapse').on('show.bs.collapse', function () {
+        $(this).prev().find('.fa').removeClass().addClass('fa fa-minus-circle');
+    });
+
+    $('.collapse').on('hide.bs.collapse', function () {
+        $(this).prev().find('.fa').removeClass().addClass('fa fa-plus-circle');
+    });
+});
