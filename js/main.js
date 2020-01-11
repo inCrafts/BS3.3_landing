97e3ab5c095a57fdf96261387516fd574aab45f3
inCrafts <inCrafts@mail.ru>
2020-01-12 00:32:35 +0300

    Валидация формы регистрации

--- js/main.js
+++ js/main.js
@@ -6,4 +6,12 @@ $(function(){
     $('.collapse').on('hide.bs.collapse', function () {
         $(this).prev().find('.fa').removeClass().addClass('fa fa-plus-circle');
     });
+
+    $('#myForm').validator({
+        feedback: {
+            success: 'fa fa-check-circle',
+            error: 'fa fa-times-circle'
+        }
+    });
+
 });
